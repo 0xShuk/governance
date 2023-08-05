f4c41dff8f6cc779aca16d4d815d3f65045896ea
0xShuk <shukarsh@gmail.com>
2023-08-05 21:21:53 +0530

    hub 2

--- hub/providers/Proposal/createProposal.ts
+++ hub/providers/Proposal/createProposal.ts
@@ -38,7 +38,7 @@ import { NFTWithMeta, VotingClient } from '@utils/uiTypes/VotePlugin';
 import { fetchPlugins } from './fetchPlugins';
 
 interface Args {
-  callbacks?: Parameters<typeof _createProposal>[11];
+  callbacks?: Parameters<typeof _createProposal>[12];
   cluster?: string;
   connection: Connection;
   councilTokenMintPublicKey?: PublicKey;
